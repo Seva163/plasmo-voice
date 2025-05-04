c3041a3411da721b2390cd32d68453b01586b2d9
Unfr <80214641+Unf3@users.noreply.github.com>
2025-05-04 03:49:04 +0300

    feat: build only for paper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,21 +37,21 @@ file("api").listFilesOrdered {
 // Common
 include("common")
 
-include("client")
-project(":client").apply {
-    projectDir = file("client/")
-    buildFileName = "root.gradle.kts"
-}
-
-file("client").listFilesOrdered {
-    return@listFilesOrdered it.isDirectory && it.name.contains("-")
-}.forEach {
-    include("client:${it.name}")
-    project(":client:${it.name}").apply {
-        projectDir = file("client/${it.name}")
-        buildFileName = "../build.gradle.kts"
-    }
-}
+// include("client")
+// project(":client").apply {
+//     projectDir = file("client/")
+//     buildFileName = "root.gradle.kts"
+// }
+//
+// file("client").listFilesOrdered {
+//     return@listFilesOrdered it.isDirectory && it.name.contains("-")
+// }.forEach {
+//     include("client:${it.name}")
+//     project(":client:${it.name}").apply {
+//         projectDir = file("client/${it.name}")
+//         buildFileName = "../build.gradle.kts"
+//     }
+// }
 
 // Server-Proxy Common (Module for common code between server and proxy implementations)
 include("server-proxy-common")
@@ -67,8 +67,8 @@ file("server").listFilesOrdered {
 }
 
 // Proxy
-file("proxy").listFilesOrdered {
-    return@listFilesOrdered it.isDirectory && it.name != "build"
-}.forEach {
-    include("proxy:${it.name}")
-}
+// file("proxy").listFilesOrdered {
+//     return@listFilesOrdered it.isDirectory && it.name != "build"
+// }.forEach {
+//     include("proxy:${it.name}")
+// }
